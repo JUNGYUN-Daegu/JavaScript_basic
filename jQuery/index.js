7d00e003d6c48ba951ae70cea055ebf285e2b8b5
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2020-12-16 18:55:05 +0900

    Add files via upload

---
+++ jQuery/index.js
@@ -0,0 +1,8 @@
+$("h1").addClass("big-title margin-50");
+$("h1").hasClass("big-title")
+
+$("h1").text("Hello World")
+$("button").html("<em>Hey</em>")
+
+// console.log($("img").attr("src"));
+$("a").attr("href", "https://www.yahoo.com")
