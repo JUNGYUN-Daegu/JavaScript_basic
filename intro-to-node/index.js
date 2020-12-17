0a375483df9e9de257a72fa9bb3af421bac51946
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2020-12-17 17:35:51 +0900

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+// const fs = require("fs")
+//
+// fs.copyFileSync('file1.txt', 'file3.txt');
+// console.log('source.txt was copied to destination.txt');
+
+var superheroes = require("superheroes");
+
+console.log(superheroes.random());
